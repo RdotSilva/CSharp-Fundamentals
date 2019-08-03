5efddad0dfad8320ccf2395a5fe610147902ec09
Ryan Silva <RyandotSilva@gmail.com>
2019-08-03 13:15:32 -0400

    Add type conversion example

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(number);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+            
+        }
+    }
+}
