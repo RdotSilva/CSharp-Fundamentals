24653ff232f6cafc5bfde8933a81afa184429323
Ryan Silva <RyandotSilva@gmail.com>
2019-08-03 13:38:58 -0400

    Add operators example

---
+++ Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+			// 3 * 3
+            Console.WriteLine((a + b)* c);
+			
+			// False
+			Console.WriteLine(!(c > b || c == a));
+        }
+    }
+}
