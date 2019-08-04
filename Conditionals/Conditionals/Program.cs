65990654ab6daf97c79b4763c6dcece4432182c7
Ryan Silva <RyandotSilva@gmail.com>
2019-08-04 15:01:19 -0400

    Add conditional example

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Spring:
+                    Console.WriteLine("Mild");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("Hot");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("Cold");
+                    break;
+            }
+        }
+    }
+}
