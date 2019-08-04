bc1175bdd74252a44bc30d6d738be3047c8229cd
Ryan Silva <RyandotSilva@gmail.com>
2019-08-04 14:09:10 -0400

    Add string examples

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Cool";
+            String lastName = "Guy";
+            string myName = "Cool Guy";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] {"John", "Jack", "Mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = "Hi John \nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            Console.WriteLine(text);
+
+            var text2 = @"Hi John Look into the following paths c:\folder1\folder2 c:\folder3\folder4";
+            Console.WriteLine(text2);
+        }
+    }
+}
