0c921fc9b1025b4ce9581abf2f2326d09ad0d7a2
Ryan Silva <RyandotSilva@gmail.com>
2019-08-05 14:15:54 -0400

    Add foreach examples

--- Iterations/Iterations/Program.cs
+++ Iterations/Iterations/Program.cs
@@ -25,6 +25,25 @@ namespace Iterations
                     Console.WriteLine(i);
                 }
             }
+
+            var name = "John Smith";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            var numbers = new int[] {1, 2, 3, 4};
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
