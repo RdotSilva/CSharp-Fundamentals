791ed8e9dcf214e5855cc1d9b3d71d8b8db478aa
Ryan Silva <RyandotSilva@gmail.com>
2019-08-05 14:45:54 -0400

    Add length example

---
+++ ArrayMethods/ArrayMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] {3, 7, 9, 2, 14, 6};
+
+            // Length
+            Console.WriteLine(numbers.Length);
+        }
+    }
+}
