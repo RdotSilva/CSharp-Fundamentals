a5158d049996da076058571a1d0789f44ec65141
Ryan Silva <RyandotSilva@gmail.com>
2019-08-05 14:23:13 -0400

    Add while loop example

--- Iterations/Iterations/Program.cs
+++ Iterations/Iterations/Program.cs
@@ -10,39 +10,63 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++)
+            //for (var i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //for (var i = 10; i >= 1; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //var name = "John Smith";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach (var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //var numbers = new int[] {1, 2, 3, 4};
+
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //var counter = 0;
+            //while (counter <= 10)
+            //{
+            //    if (counter % 2 == 0)
+            //    {
+            //        Console.WriteLine(counter);
+            //        counter++;
+            //    }
+            //}
+
+            while (true)
             {
-                if (i % 2 == 0)
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
                 }
-            }
 
-            for (var i = 10; i >= 1; i--)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-
-            var name = "John Smith";
-
-            for (var i = 0; i < name.Length; i++)
-            {
-                Console.WriteLine(name[i]);
-            }
-
-            foreach (var character in name)
-            {
-                Console.WriteLine(character);
-            }
-
-            var numbers = new int[] {1, 2, 3, 4};
-
-            foreach (var number in numbers)
-            {
-                Console.WriteLine(number);
+                break;
             }
         }
     }
