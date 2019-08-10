4c84e1e75184d4ae0243db75e97e04604acc444a
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 14:05:26 -0400

    Add directory examples

---
+++ Directory/Directory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory((@"c:\temp\folder1"));
+
+            var files = Directory.GetFiles(@"c:\temp\folder1", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"c:\temp\folder1", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Console.WriteLine(Directory.Exists(@"c:\testingthisout"));
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
