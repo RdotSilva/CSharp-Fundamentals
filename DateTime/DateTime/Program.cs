a8e6947613b1c2ce2e6186aefb74b0085a4ac659
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 12:22:13 -0400

    Add DateTime examples

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+        }
+    }
+}
