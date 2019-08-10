c64d9e8a44c88cc98bb35c9158a3d3e136a71f6c
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 14:14:03 -0400

    Add path method examples

---
+++ PathMethods/PathMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\test\test";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: :" + Path.GetExtension(path));
+            Console.WriteLine("File Name: :" + Path.GetFileName(path));
+            Console.WriteLine("File Name w/o extensions :" + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
