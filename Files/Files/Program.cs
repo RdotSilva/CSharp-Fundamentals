cb301820e719905f4e1c2d12b54203605f1f73ae
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 13:57:01 -0400

    Add file examples

---
+++ Files/Files/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            // Static methods
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+
+            var content = File.ReadAllText(path);
+
+            // Instance methods
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+
+            if (fileInfo.Exists)
+            {
+                //
+            }
+            fileInfo.Read
+        }
+    }
+}
