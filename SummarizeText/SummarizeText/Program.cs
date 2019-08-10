7e0178789fbf47aa1dba9a91048b14ea3544ecbb
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 13:11:13 -0400

    Add Summarize Text example

---
+++ SummarizeText/SummarizeText/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizeText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really long text that we want to summarize.";
+            var summary = StringUtility.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
+
+        }
+
+    }
+}
