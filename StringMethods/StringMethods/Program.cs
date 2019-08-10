3f7ed25358d27d5b49cd72f4e31fa41416b6c887
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 12:54:00 -0400

    Add string method examples

---
+++ StringMethods/StringMethods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullname = "John Smith ";
+            Console.WriteLine("Trim: '{0}'", fullname.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullname.Trim().ToUpper());
+
+            var index = fullname.IndexOf(' ');
+            var firstName = fullname.Substring(0, index);
+            var lastName = fullname.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullname.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullname.Replace("John", "Jonathan"));
+
+            if (String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("Invalid");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0"));
+        }
+    }
+}
